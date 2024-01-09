ce1ba22068599757cf9723f15880426702e79837
QuiLion7 <quilbrub@gmail.com>
2024-01-09 11:59:24 -0300

    feat(home): Update dimensions of the parent div on the home page and Add components to home page - Banner, About, Services, Work, Mentoring, Contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import About from "@/components/about";
+import Banner from "@/components/banner";
+import Contact from "@/components/contact";
+import Mentoring from "@/components/mentoring";
 import Nav from "@/components/nav";
+import Services from "@/components/services";
+import Work from "@/components/work";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-hidden bg-[url('/assets/site-bg.jpg')] bg-cover bg-center bg-no-repeat ">
-      oi
-      <Nav />
+    <div className="bg-[url('/assets/site-bg.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="3xl:container w-full px-2">
+        <Banner />
+        <Nav />
+        <About />
+        <Services />
+        <Work />
+        <Mentoring />
+        <Contact />
+        <div className="h-[4000px]"></div>
+      </div>
     </div>
   );
 }
