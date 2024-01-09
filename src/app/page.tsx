d9ea86d787650ca8655fca4d0499156e8348be88
QuiLion7 <quilbrub@gmail.com>
2024-01-09 01:43:48 -0300

    feat(home): Integrate Nav component into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Nav from "@/components/nav";
 
 export default function Home() {
-  return <div>Olá</div>;
+  return (
+    <div className="h-screen overflow-hidden bg-[url('/assets/site-bg.jpg')] bg-cover bg-center bg-no-repeat ">
+      oi
+      <Nav />
+    </div>
+  );
 }
