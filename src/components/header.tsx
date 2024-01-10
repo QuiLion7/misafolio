0ccc305b9cb471f2b3a2a80c2b6086f37dde8201
QuiLion7 <quilbrub@gmail.com>
2024-01-10 00:52:17 -0300

    chore(header): Change text gradient in the Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
             className="h-[45px] w-auto"
             alt="Picture of the author"
           /> */}
-        <h1 className="bg-gradient-to-r from-purple-900 to-indigo-500 bg-clip-text text-sm font-bold text-transparent md:text-base lg:text-lg">
+        <h1 className="bg-gradient-to-r from-purple-900 via-purple-600 to-violet-600 bg-clip-text text-sm font-bold text-transparent md:text-base lg:text-lg">
           MISAEL TRADER
         </h1>
       </div>
