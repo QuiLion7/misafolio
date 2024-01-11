078b87f70e52a6d72036476b6a2fb56ea12fb2dd
QuiLion7 <quilbrub@gmail.com>
2024-01-11 10:29:31 -0300

    feat(contact): Add content, including a contact form, to the Contact component

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,183 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/variants";
+import { TypeAnimation } from "react-type-animation";
+
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+
+const profileFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      message: "O seu nome deve ter pelo menos 3 caracteres.",
+    })
+    .max(30, {
+      message: "O seu nome não deve ter mais de 30 caracteres.",
+    }),
+  email: z
+    .string({
+      required_error: "Escreva seu E-mail",
+    })
+    .email(),
+  message: z
+    .string()
+    .max(160, {
+      message: "Sua mensagem não deve ter mais 160 caracteres.",
+    })
+    .min(7, {
+      message: "Sua mensagem deve ter pelo menos 7 caracteres.",
+    }),
+});
+
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
+
 export default function Contact() {
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(data: ProfileFormValues) {
+    const messageWithUserInfo = `Site\n\nNome: ${data.name}\nE-mail: ${data.email}\nMensagem: ${data.message}`;
+
+    const whatsappUrl = `https://wa.me/5534991184741?text=${encodeURIComponent(
+      messageWithUserInfo,
+    )}`;
+
+    window.open(whatsappUrl, "_blank");
+
+    form.reset();
+  }
+
   return (
-    <div id="contact" className="flex items-center py-8 lg:h-screen lg:py-24">
-      Contact
-    </div>
+    <section
+      id="contact"
+      className="flex h-svh w-full flex-col items-center justify-center"
+    >
+      <div className="flex h-full w-full flex-col items-center justify-center gap-2 md:flex-row">
+        <div className="flex h-[250px] w-full flex-col items-center justify-end pb-8 sm:h-[350px] sm:justify-center md:h-full md:flex-1">
+          <div className="flex w-full max-w-[500px] flex-col items-center justify-center gap-1 md:gap-4">
+            <motion.h3
+              variants={fadeIn("right", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-lg font-bold uppercase tracking-wide text-primary md:text-xl lg:text-2xl"
+            >
+              Entre em Contato
+            </motion.h3>
+            <motion.div
+              variants={fadeIn("right", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex-col text-center text-3xl font-bold uppercase md:text-4xl lg:text-5xl"
+            >
+              <h1>Vamos estudar </h1>
+              <TypeAnimation
+                sequence={["Juntos!", 2000, "Forex!", 2000]}
+                speed={50}
+                className="text-primary"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+          </div>
+        </div>
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex h-full w-full items-start justify-center px-4 sm:items-center md:flex-1 md:justify-start"
+        >
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full max-w-[500px] space-y-7 rounded-xl border-2 border-primary/30 p-4 duration-300 hover:border-primary/100"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Seu nome"
+                        {...field}
+                        className="rounded-xl bg-background/20 focus:border-b-2"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>E-mail</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Seu e-mail"
+                        {...field}
+                        className="rounded-xl bg-background/20 focus:border-b-2"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Mensagem</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Conte-me um pouco da solução que deseja..."
+                        className="resize-none rounded-xl bg-background/20 focus:border-b-2"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="flex w-full items-center justify-center">
+                <Button
+                  type="submit"
+                  className="w-full max-w-[250px] cursor-pointer rounded-xl bg-gradient-to-r from-purple-900 via-purple-600 to-violet-600 font-bold uppercase text-white"
+                >
+                  Enviar
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </motion.div>
+      </div>
+    </section>
   );
 }
